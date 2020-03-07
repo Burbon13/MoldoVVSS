58fb74f1c0c900d71b56ac35ff35305a52da2829
Burbon13 <razvanroatis91@gmail.com>
2020-03-07 15:07:11 +0200

    Refactored services

--- src/main/java/tasks/services/DateService.java
+++ src/main/java/tasks/services/DateService.java
@@ -1,8 +1,6 @@
 package tasks.services;
 
 
-import org.apache.log4j.Logger;
-
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
@@ -14,37 +12,41 @@ public class DateService {
     public static final int SECONDS_IN_MINUTE = 60;
     public static final int MINUTES_IN_HOUR = 60;
     public static final int HOURS_IN_A_DAY = 24;
-    private static final Logger log = Logger.getLogger(DateService.class.getName());
 
     private TasksService service;
 
-    public DateService(TasksService service){
-        this.service=service;
+    public DateService(TasksService service) {
+        this.service = service;
     }
-    public static LocalDate getLocalDateValueFromDate(Date date){//for setting to DatePicker - requires LocalDate
+
+    public static LocalDate getLocalDateValueFromDate(Date date) {//for setting to DatePicker - requires LocalDate
         return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
 
     }
-    public Date getDateValueFromLocalDate(LocalDate localDate){//for getting from DatePicker
-        if (localDate == null || localDate.equals(""))
+
+    public Date getDateValueFromLocalDate(LocalDate localDate) {//for getting from DatePicker
+        if (localDate == null)
             throw new NullPointerException("date is invalid");
         Instant instant = Instant.from(localDate.atStartOfDay(ZoneId.systemDefault()));
         return Date.from(instant);
     }
+
     public Date getDateMergedWithTime(String time, Date noTimeDate) {//to retrieve Date object from both DatePicker and time field
         if (!time.matches("[0-9][0-9]?:[0-9][0-9]") || time.matches(""))
             throw new IllegalArgumentException("minutes or seconds are invalid");
         String[] units = time.split(":");
         int hour = Integer.parseInt(units[0]);
         int minute = Integer.parseInt(units[1]);
-        if (hour > HOURS_IN_A_DAY || minute > MINUTES_IN_HOUR) throw new IllegalArgumentException("time unit exceeds bounds");
+        if (hour > HOURS_IN_A_DAY || minute > MINUTES_IN_HOUR)
+            throw new IllegalArgumentException("time unit exceeds bounds");
         Calendar calendar = GregorianCalendar.getInstance();
         calendar.setTime(noTimeDate);
         calendar.set(Calendar.HOUR_OF_DAY, hour);
         calendar.set(Calendar.MINUTE, minute);
         return calendar.getTime();
     }
-        public String getTimeOfTheDayFromDate(Date date){//to set in detached time field
+
+    public String getTimeOfTheDayFromDate(Date date) {//to set in detached time field
         Calendar calendar = GregorianCalendar.getInstance();
         calendar.setTime(date);
         int hours = calendar.get(Calendar.HOUR_OF_DAY);
