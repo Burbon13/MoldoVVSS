38b0e444dbe08d736da053986d29114cab47cf53
Burbon13 <razvanroatis91@gmail.com>
2020-04-05 12:53:17 +0300

    Implemented last 2 tests and refactored the test class

--- src/main/java/tests/TasksOperationsTest.java
+++ src/main/java/tests/TasksOperationsTest.java
@@ -2,85 +2,94 @@ package tests;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.fxml.FXMLLoader;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import tasks.model.ArrayTaskList;
 import tasks.model.Task;
 import tasks.model.TasksOperations;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+@SuppressWarnings("Duplicates")
 class TasksOperationsTest {
 
-    ObservableList<Task> tasks;
-    TasksOperations tasksOperations;
+    private ObservableList<Task> tasks;
+    private Calendar calendar;
+    private TasksOperations tasksOperations;
+    private List<Task> all;
+    private Date startDate;
+    private Date stopDate;
+    private Task task1;
+    private Task task2;
 
-    @Test
-    public void F02_TC03(){
-        ArrayList<Task> all=new ArrayList();
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(2020, 4, 4, 8, 20, 0);
-        Date startDate = calendar.getTime();
-        calendar.set(2020, 4, 5, 8, 20, 0);
-        Date stopDate= calendar.getTime();
-        Task task1=new Task("Task1",startDate,stopDate,2);
+
+    @BeforeEach
+    public void beforeEachTest() {
+        calendar = Calendar.getInstance();
         calendar.set(2020, 4, 10, 8, 20, 0);
         startDate = calendar.getTime();
         calendar.set(2020, 4, 11, 8, 20, 0);
-        stopDate= calendar.getTime();
-        Task task2=new Task("Task2",startDate,stopDate,2);
-        all.add(task1);
-        all.add(task2);
-        tasks= FXCollections.observableArrayList(all);
-        tasksOperations=new TasksOperations(tasks);
+        stopDate = calendar.getTime();
+        task2 = new Task("Task2", startDate, stopDate, 2);
+        all = new ArrayList<>();
         calendar.set(2020, 4, 3, 8, 20, 0);
         startDate = calendar.getTime();
         calendar.set(2020, 4, 8, 8, 20, 0);
-        stopDate= calendar.getTime();
-        Iterable<Task> result=tasksOperations.incoming(startDate,stopDate);
-        result.forEach(x->{
-            if(!x.getTitle().equals("Task1"))
-                assert false;
+        stopDate = calendar.getTime();
+        task1 = new Task("Task1", startDate, stopDate, 2);
+    }
+
+    @Test
+    public void FO2_TCO1_WHEN_NoTasks_THEN_EmptyListReturned() {
+        tasks = FXCollections.observableArrayList(all);
+        tasksOperations = new TasksOperations(tasks);
+        Iterable<Task> result = tasksOperations.incoming(startDate, stopDate);
+        result.forEach(x -> {
+            assert false;
         });
         assert true;
     }
 
     @Test
-    public void F02_TC04(){
-        ArrayList<Task> all=new ArrayList();
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(2020, 4, 1, 8, 20, 0);
-        Date startDate = calendar.getTime();
-        calendar.set(2020, 4, 5, 8, 20, 0);
-        Date stopDate= calendar.getTime();
-        Task task1=new Task("Task1",startDate,stopDate,2);
-        calendar.set(2020, 4, 10, 8, 20, 0);
-        startDate = calendar.getTime();
+    public void FO2_TCO2_WHEN_OneTaskInInterval_THEN_OneTaskReturned() {
         calendar.set(2020, 4, 11, 8, 20, 0);
-        stopDate= calendar.getTime();
-        Task task2=new Task("Task2",startDate,stopDate,2);
+        all.add(task1);
+        tasks = FXCollections.observableArrayList(all);
+        tasksOperations = new TasksOperations(tasks);
+        Iterable<Task> result = tasksOperations.incoming(startDate, stopDate);
+        result.forEach(x -> {
+            assert x.getTitle().equals("Task1");
+        });
+        assert true;
+    }
+
+    @Test
+    public void F02_TC03_WHEN_TwoTasksSaved_AND_OneIsInInterval_THEN_OneTaskReturned() {
+        all.add(task1);
+        all.add(task2);
+        tasks = FXCollections.observableArrayList(all);
+        tasksOperations = new TasksOperations(tasks);
+        Iterable<Task> result = tasksOperations.incoming(startDate, stopDate);
+        result.forEach(x -> {
+            assert x.getTitle().equals("Task1");
+        });
+        assert true;
+    }
+
+    @Test
+    public void F02_TC04_WHEN_NoTaskInInterval_THEN_NoTasksReturner() {
         task2.setStartTime(null);
         all.add(task1);
         all.add(task2);
-        tasks= FXCollections.observableArrayList(all);
-        tasksOperations=new TasksOperations(tasks);
-        calendar.set(2020, 4, 3, 8, 20, 0);
-        startDate = calendar.getTime();
-        calendar.set(2020, 4, 8, 8, 20, 0);
-        stopDate= calendar.getTime();
-        Iterable<Task> result=tasksOperations.incoming(startDate,stopDate);
-        result.forEach(x->{
-            if(x.getTitle().equals("Task1") || x.getTitle().equals("Task2"))
-                assert false;
+        tasks = FXCollections.observableArrayList(all);
+        tasksOperations = new TasksOperations(tasks);
+        Iterable<Task> result = tasksOperations.incoming(startDate, stopDate);
+        result.forEach(x -> {
+            assert !x.getTitle().equals("Task1") && !x.getTitle().equals("Task2");
         });
         assert true;
     }
-
-
 }
