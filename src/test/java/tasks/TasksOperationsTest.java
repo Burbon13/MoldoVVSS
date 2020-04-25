cabb3dfc359365d0091bf1ecf1fe205538b51202
Robu Vlad <38671514+robuvlad@users.noreply.github.com>
2020-04-25 20:01:05 +0300

    moved tests to java test directory

--- src/test/java/tasks/TasksOperationsTest.java
+++ src/test/java/tasks/TasksOperationsTest.java
@@ -1,4 +1,4 @@
-package tests;
+package tasks;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
--- src/test/java/tasks/TasksServiceTest.java
+++ src/test/java/tasks/TasksServiceTest.java
@@ -1,4 +1,4 @@
-package tests;
+package tasks;
 
 import org.junit.jupiter.api.*;
 import tasks.model.ArrayTaskList;
